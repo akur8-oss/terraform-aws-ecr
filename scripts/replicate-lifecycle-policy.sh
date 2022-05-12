5be6265cd4c3377c7cee743bd73953690d2e7746
Theo Jammes-Beuve <theo.jammes@akur8.com>
2022-05-12 12:12:19 +0200

    fix: Script does not detect correctly if replicate exist before applying changes

--- scripts/replicate-lifecycle-policy.sh
+++ scripts/replicate-lifecycle-policy.sh
@@ -7,26 +7,29 @@ printf "%s/%s - Retrieve lifecycle policy from source repository.\n" "${REPOSITO
 aws ecr \
   get-lifecycle-policy \
   --repository-name "${REPOSITORY}" \
-  --region "${CURRENT_REGION}" \
-  | jq 'del(.lastEvaluatedAt)' \
-  > "${lifecycle_policy}"
+  --region "${CURRENT_REGION}" |
+  jq 'del(.lastEvaluatedAt)' \
+  >"${lifecycle_policy}"
 
-{
+should_be_replicated=$(aws ecr list-images --repository-name "${REPOSITORY}" --region "${CURRENT_REGION}" | jq -r ".imageIds | length")
+
+if [ "${should_be_replicated}" -gt 1 ]; then
   aws ecr \
     describe-repositories \
     --repository-names "${REPOSITORY}" \
     --region "${REGION}" \
-    > /dev/null 2>&1
-  printf "%s/%s - Repository exist in region.\n" "${REPOSITORY}" "${REGION}"
+    >/dev/null 2>&1
+  printf "%s/%s - Repository exists in region.\n" "${REPOSITORY}" "${REGION}"
   printf "%s/%s - Applying lifecycle policy.\n" "${REPOSITORY}" "${REGION}"
-} && {
   aws ecr \
     put-lifecycle-policy \
     --repository-name "${REPOSITORY}" \
     --region "${REGION}" \
     --cli-input-json "file://${lifecycle_policy}" \
-    > /dev/null 2>&1
-  printf "%s/%s - Policy applied.\n" "${REPOSITORY}" "${REGION}"
-}
+    >/dev/null 2>&1
+   printf "%s/%s - Policy applied.\n" "${REPOSITORY}" "${REGION}"
+else
+  printf "%s/%s - Repository is empty in %s and not replicated. Skipping.\n" "${REPOSITORY}" "${REGION}" "${CURRENT_REGION}"
+fi
 
 rm -f "${lifecycle_policy}"

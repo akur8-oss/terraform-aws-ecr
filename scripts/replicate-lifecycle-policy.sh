473fe1be06133d15538b1694dfac0fbf59b4706e
Theo Jammes-Beuve <theo.jammes@akur8.com>
2022-07-04 11:05:16 +0200

    shellcheck

--- scripts/replicate-lifecycle-policy.sh
+++ scripts/replicate-lifecycle-policy.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -e
 
+# shellcheck disable=SC2046,SC2183
+assume_role() {
+  if [[ -n "${ROLE_TO_ASSUME}" ]]; then
+  export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+    $(aws sts assume-role \
+    --role-arn "${ROLE_TO_ASSUME}" \
+    --role-session-name "terraform-aws-ecr" \
+    --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+    --output text))
+    fi
+}
+
 lifecycle_policy=$(mktemp)
 
 printf "%s/%s - Retrieve lifecycle policy from source repository.\n" "${REPOSITORY}" "${CURRENT_REGION}"
 
-if [[ -n "${ROLE_TO_ASSUME}" ]]; then
-  export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-  $(aws sts assume-role \
-  --role-arn "${ROLE_TO_ASSUME}" \
-  --role-session-name "terraform-aws-ecr" \
-  --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
-  --output text))
-fi
+assume_role
 
 aws ecr \
   get-lifecycle-policy \

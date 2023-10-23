2700833e432acba77399c332d4279f33edd367f9
Theo Jammes-Beuve <theo.jammes@akur8.com>
2023-10-23 17:48:51 +0200

    betterfix?

--- main.tf
+++ main.tf
@@ -21,14 +21,14 @@ resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
 }
 
 resource "null_resource" "replicate_lifecycle_policy" {
-  for_each = length(var.lifecycle_policy) > 0 ? tomap({ for idx, v in var.replicated_region : idx => v }) : tomap({})
+  for_each = length(var.lifecycle_policy) > 0 ? tolist(var.replication_region) : tolist([])
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/replicate-lifecycle-policy.sh"
     environment = {
       CURRENT_REGION = data.aws_region.current.name
       REPOSITORY     = var.name
-      REGION         = each.value
+      REGION         = each.key
       ROLE_TO_ASSUME = var.role_to_assume
     }
   }
@@ -51,14 +51,14 @@ resource "aws_ecr_repository_policy" "repository_policy" {
 }
 
 resource "null_resource" "replicate_repository_policy" {
-  for_each = length(var.lifecycle_policy) > 0 ? tomap({ for idx, v in var.replicated_region : idx => v }) : tomap({})
+  for_each = length(var.lifecycle_policy) > 0 ? tolist(var.replicated_region) : tolist([])
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/replicate-repository-policy.sh"
     environment = {
       CURRENT_REGION = data.aws_region.current.name
       REPOSITORY     = var.name
-      REGION         = each.value
+      REGION         = each.key
       ROLE_TO_ASSUME = var.role_to_assume
     }
   }
